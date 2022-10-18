cd0a5debda15eaa273ccb7b6265260bb31976d58
zy <953725892@qq.com>
2022-10-18 19:31:42 +0800

    完成插件注册部分代码编写

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"netbalance/plugin"
+
+	"k8s.io/component-base/logs"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	command := app.NewSchedulerCommand(
+		app.WithPlugin(plugin.Name, plugin.New),
+	)
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+	if err := command.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
